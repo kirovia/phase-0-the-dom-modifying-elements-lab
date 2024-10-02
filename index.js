22549005756f3f526cfba9ff9a0bb55b7f4082aa
Joseph Andrew Phillips <josephandrewphillips@gmail.com>
2024-10-02 10:37:20 -0400

    passes all tests

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+const main = document.getElementById("main");
+main.remove();
+const newHeader = document.createElement("h1");
+newHeader.textContent = "Jojo is the champion";
+newHeader.id = "victory";
+document.body.append(newHeader);
